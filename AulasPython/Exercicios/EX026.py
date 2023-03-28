0551978968fd51eefed1f1f975ed88b74778bbdd
Fábio De Souza Carvalho <fabiodesouza733@gmail.com>
2023-03-28 01:47:39 -0300

    Update EX026.py

--- AulasPython/Exercicios/EX026.py
+++ AulasPython/Exercicios/EX026.py
@@ -1,2 +1,10 @@
-#
-#
+#Faça um exercicio que leia ma frase pelo teclado e mostre:
+#Quantas vezes aparece a letra 'A'
+#Em que posição ele aparece pela primeira vez 
+#Em que posição ele aparece pela última vez
+
+nome =str(input('Digite uma frase: ')).strip().upper()
+
+print('A letra A aparece {} vezes na frase'.format(nome.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(nome.find('A')+1))
+print('A ukltima posição apareceu na posição {}'.format(nome.rfind('A')+1))
