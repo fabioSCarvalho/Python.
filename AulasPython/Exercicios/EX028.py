68edb7dd42e299a8862ee4bc66786857b2104df5
Fábio De Souza Carvalho <fabiodesouza733@gmail.com>
2023-04-08 03:10:54 -0300

    exercicios condição if

---
+++ AulasPython/Exercicios/EX028.py
@@ -0,0 +1,15 @@
+#Escreva um programa que faça o computador pensar em numero entre 0 e 5
+#e peça para o  usuario tentar descobrir o numero escolhido
+#o programa deverá escrever na tela se o usuario perdeu ou venceu
+
+from random import randint
+sorteado = randint(0,5)
+
+print('sorteando...')
+
+n = int(input('Tente adivinhar o numero sorteado entre 0 e 5 :'))
+
+if n == sorteado:
+    print('Você ganhou')
+else:
+    print('Você perdeu')
